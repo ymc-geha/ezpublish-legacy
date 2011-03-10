18e68cf686ec5724ff2d5880bce43462309b4f61
Patrick Allaert <pa@ez.no>
2011-03-10 01:07:34 +0100

    Removed: Previously deprecated functions from eZContentOperationCollection class

--- tests/tests/kernel/classes/ezcontentoperationcollection_regression.php
+++ tests/tests/kernel/classes/ezcontentoperationcollection_regression.php
@@ -18,67 +18,6 @@ class eZContentOperationCollectionRegression extends ezpDatabaseTestCase
     }
 
     /**
-     * Test to verify that eznode_assignment entres get removed.
-     *
-     * @link http://issues.ez.no/15478
-     */
-    public function testRemoveAssignments()
-    {
-        $db = eZDB::instance();
-
-        $testSet = array();
-
-        $folder = new ezpObject( "folder", 2 );
-        $folder->name = __FUNCTION__;
-        $folder->publish();
-        $testSet[] = $folder;
-
-        $childOne = new ezpObject( "folder", $folder->mainNode->node_id );
-        $childOne->name = "ChildOne";
-        $childOne->publish();
-        $testSet[] = $childOne;
-
-        $childTwo = new ezpObject( "folder", $folder->mainNode->node_id );
-        $childTwo->name = "ChildTwo";
-        $childTwo->publish();
-        $testSet[] = $childTwo;
-
-        $subChildOne = new ezpObject( 'article', $childOne->mainNode->node_id );
-        $subChildOne->title = "SubChild";
-        $subChildOne->publish();
-        $testSet[] = $subChildOne;
-
-        $subChildTwo = new ezpObject( 'article', $childOne->mainNode->node_id );
-        $subChildTwo->title = "SubChildOther";
-        $subChildTwo->publish();
-        $testSet[] = $subChildTwo;
-
-        $subChildThree = new ezpObject( 'article', $childOne->mainNode->node_id );
-        $subChildThree->title = "SubChildThird";
-        $subChildThree->publish();
-        $testSet[] = $subChildThree;
-
-        // Let's add another placement here
-        $newPlacementSubChildOne = $subChildOne->addNode( $childTwo->mainNode->node_id );
-        $testSet[] = $newPlacementSubChildOne;
-
-        // $this->debugBasicNodeInfo( $testSet );
-
-        $coId = $newPlacementSubChildOne->attribute( 'contentobject_id' );
-
-        eZContentOperationCollection::removeAssignment( $childTwo->mainNode->node_id, $childTwo->id, array( $newPlacementSubChildOne ), false );
-
-        $checkSql = "SELECT * FROM eznode_assignment WHERE contentobject_id = {$coId}";
-        $result = $db->arrayQuery( $checkSql );
-
-        // After removing one of the assignments, the number of node
-        // assignments for the content object should be only one.
-        $numberOfAssignments = count( $result );
-
-        self::assertEquals( 1, $numberOfAssignments, "The number of node assignments are not correct, eznode_assignment table not cleaned up correctly." );
-    }
-
-    /**
      * Helper method to aid the development and test and verification of results.
      *
      * The method will output information about the inputted objects and nodes
